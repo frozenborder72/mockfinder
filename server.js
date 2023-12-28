6bc4cbbc2be4e8e779316c5d5af17e1c303c801a
Marco Manunta <mar.manunta@gmail.com>
2023-12-28 11:32:38 +0100

    add .env file and basic express server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('SERVER')
+const express = require('express')
+require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/records', (req, res) => {
+  res.status(201).json({ message: 'Hello' })
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
