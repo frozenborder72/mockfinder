ec0a24d4b2e8d1ced0652bade198d6f0a2ea1464
Marco Manunta <mar.manunta@gmail.com>
2023-12-28 17:08:29 +0100

    add connection to db

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 require('dotenv').config()
-const PORT = process.env.PORT || 5000
+require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+
+// connect to database
+connectDB()
 
 const app = express()
 
